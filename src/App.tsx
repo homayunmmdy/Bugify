d372ab2445345f438b61b1c3136621d7888a63e4
Homayoun <homayoun763@gmail.com>
2025-08-07 14:27:15 +0330

    add : react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-const App = () => {
+import { Toaster } from "react-hot-toast";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./layout/Main";
+import ErrorPage from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+    // loader: mainLoader,
+    errorElement: <ErrorPage />
+  },
+]);
+
+function App() {
   return (
-    <div>Hello World</div>
-  )
+    <div className="App">
+      <RouterProvider router={router} />
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {}, // Just enables SCSS without additional config
+    },
+  },
   plugins: [react()],
-})
+});
