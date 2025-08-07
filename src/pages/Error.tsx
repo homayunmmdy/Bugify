198a63461ed2db1c980408461ef9031f9414c977
Homayoun <homayoun763@gmail.com>
2025-08-07 15:09:08 +0330

    add : style 404 page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,25 @@
-const ErrorPage = () => {
-  return <div>ErrorPage</div>;
-};
+import './Error.scss'
+import { BiArrowBack, BiHome } from "react-icons/bi";
+import { Link, useNavigate } from "react-router-dom";
 
+const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="error">
+      <h1>Uh oh! We’ve got a problem.</h1>
+      <p>404</p>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <BiArrowBack width={20} />
+          <span>Go Back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <BiHome width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default ErrorPage;
