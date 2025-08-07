74b572faeab712f0d0039583d9726c31de3f5ef8
Homayoun <homayoun763@gmail.com>
2025-08-07 14:53:47 +0330

    add : bottom wave

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,9 +1,15 @@
+import "./Main.scss"
+import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
 
 const MainPage = () => {
   return (
-    <div>
+    <div className="layout">
       <Nav />
+      <main>
+        <Outlet />
+      </main>
+      <img src="wave.svg" alt="Image of wave" width={1521} height={141}/>
     </div>
   );
 };
